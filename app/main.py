bbdf034c0dc1d72d8a00a2165890af30fd1684c2
miguel-merlin <mmerlin@stevens.edu>
2024-03-30 15:14:28 -0400

    feat: Added CORS

--- app/main.py
+++ app/main.py
@@ -1,5 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.story_routes import router as story_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],
+)
+
 app.include_router(story_router)
