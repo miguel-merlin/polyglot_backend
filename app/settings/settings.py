6debf8a4827bcd141885f01e56f20599179d8285
miguel-merlin <mmerlin@stevens.edu>
2024-03-30 12:03:21 -0400

    feat: Deleted settings.py

--- app/settings/settings.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-env_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '.env'))
-load_dotenv(env_path)
-MONGO_URL = os.getenv("URI")
-DATABASE_NAME = "polyglot"
-COLLECTION_NAME = "story"
